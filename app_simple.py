0712aefba3f5b2353965436359c3e62133da3a00
Sarthak Pattnaik <dps.sarthak@gmail.com>
2025-08-19 14:21:47 -0400

    Add cloud deployment configs

--- app_simple.py
+++ app_simple.py
@@ -98,6 +98,9 @@ def get_podcast_count():
         'message': 'Mock data - deployment successful!'
     })
 
+# For Vercel deployment
+app_instance = app
+
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=False)
